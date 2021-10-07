33d65bdc2e929cd4cd197930f246dd99249032dc
Matthew Myrick <matthewmyrick2@gmail.com>
2021-10-07 14:59:58 -0500

    update

--- reactapp/src/components/header.js
+++ reactapp/src/components/header.js
@@ -1,31 +1,42 @@
 import React from 'react'
 import {Navbar, Nav} from 'react-bootstrap'
 import {LinkContainer} from 'react-router-bootstrap'
+import '../css/Header.css'
 
 const header = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <LinkContainer to="/">
-                <Navbar.Brand>Matthew Myrick</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <LinkContainer to="/">
-                        <Nav.Link>Home</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/portfolio">
-                        <Nav.Link>Portfolio</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/about">
-                        <Nav.Link>About Me</Nav.Link>
-                    </LinkContainer>
-                    <LinkContainer to="/contact">
-                        <Nav.Link>Contact</Nav.Link>
-                    </LinkContainer>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <div className="header-container">
+            <Navbar expand="lg" className='Header'>
+                {/* <LinkContainer to="/">
+                    <Navbar.Brand>Matthew Myrick</Navbar.Brand>
+                </LinkContainer> */}
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="m-auto">
+                        <LinkContainer to="/" className="navItem">
+                            <Nav.Link>
+                                Home
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/portfolio" className="navItem">
+                            <Nav.Link>
+                                Portfolio
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/about" className="navItem">
+                            <Nav.Link>
+                                About Me
+                            </Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/contact" className="navItem">
+                            <Nav.Link>
+                                Contact
+                            </Nav.Link>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        </div>
     )
 }
 
--- reactapp/src/pages/Home.js
+++ reactapp/src/pages/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className="App">
+    <div className="Home">
       <h1>This is Homepage</h1>
     </div>
   );
